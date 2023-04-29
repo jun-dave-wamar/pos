08d51dd842ac8fd6b6333ddd715e584339c1b81f
jun-dave-wamar <jundavewamar@gmail.com>
2023-04-29 17:39:21 +0800

    Removed get

--- index.js
+++ index.js
@@ -25,13 +25,13 @@ app.use(express.json())
 const {validateToken} = require("./middleware/auth");
 
 //Routes
-app.post("/api/login", login);
-app.post("/api/register", register);
-app.post("/api/products", validateToken, postProduct);
+// app.post("/api/login", login);
+// app.post("/api/register", register);
+// app.post("/api/products", validateToken, postProduct);
 
-app.get("/api/products", validateToken, getProduct);
-app.get("/api/logout", logout);
-app.get("/api/users", validateToken, getUsers);
+// app.get("/api/products", validateToken, getProduct);
+// app.get("/api/logout", logout);
+// app.get("/api/users", validateToken, getUsers);
 
 app.get("/", async (req, res) => {
   res.json({
