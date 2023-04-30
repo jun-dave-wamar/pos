2017557dc178e198f8902dd3ab97906b4f72e59e
jun-dave-wamar <jundavewamar@gmail.com>
2023-04-30 11:51:08 +0800

    Version 0.0.1

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const {getCustomer, postCustomer} = require("./api/controllers/Customer");
 
 const corsOptions = {
   //origin: ["http://localhost:3001", "http://localhost:3000"],
-   origin: ["https://v001.are-ai.ae", "https://v001.are-ai.ae/", "https://are-ai.ae", "https://are-ai.ae/"],
+   origin: ["https://snack.expo.dev/", "https://snack.expo.dev"],
    method: ["GET", "POST", "PATCH", "PUT", "HEAD", "OPTIONS"],
    credentials: true,
 };
