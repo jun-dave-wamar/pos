14bb046a5ea0db8579296436bf8a8f96345f9faf
jun-dave-wamar <jundavewamar@gmail.com>
2023-05-03 02:26:34 +0800

    Fixed product

--- api/controllers/Product.js
+++ api/controllers/Product.js
@@ -16,12 +16,12 @@ async function getProduct(req, res){
 //POST /products
 async function postProduct(req, res){
     try{
-        const {productName, price, stocks} = req.body;
+        const {productName, price, stock} = req.body;
 
         const product = new Product({
             productName,
             price,
-            stocks,
+            stock,
         });
         await product.save();
         res.status(201).json(product);
