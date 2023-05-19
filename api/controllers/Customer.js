7fa200e84eeb3b364363ee2f20f6ea2b3c2092ae
jun-dave-wamar <jundavewamar@gmail.com>
2023-05-19 14:36:46 +0800

    Added OR for product 2

--- api/controllers/Customer.js
+++ api/controllers/Customer.js
@@ -78,7 +78,7 @@ async function updateCustomer(req, res){
   
       const customerProducts = Array.isArray(product) ? product.map((item) => {
         return {
-          selectedProduct: item.selectedProduct || productName,
+          selectedProduct: item.selectedProduct || item.productName,
           productPrice: item.productPrice,
           quantity: item.quantity,
           id: item._id
